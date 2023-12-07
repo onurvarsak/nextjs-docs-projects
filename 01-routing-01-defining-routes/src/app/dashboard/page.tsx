410d77570de09e2baa0c6c4c458ff57606e8fa6d
Onur Varsak <onurvarsak@Onur-MacBook-Pro.local>
2023-12-07 11:48:55 +0300

    added - character

--- 01-routing-01-defining-routes/src/app/dashboard/page.tsx
+++ 01-routing-01-defining-routes/src/app/dashboard/page.tsx
@@ -3,8 +3,7 @@ import Link from "next/link";
 export default function Page() {
   return (
     <div style={{ display: "flex", flexDirection: "column", gap: 20 }}>
-      Dashboard Page
-      <Link href={"/"}>Back</Link>
+      Dashboard Page -<Link href={"/"}>Back</Link>
     </div>
   );
 }
