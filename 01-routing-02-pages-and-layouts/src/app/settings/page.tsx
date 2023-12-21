ec2a88cda0fc49fafff6a897548de34d5359ca5a
onurvarsak <onurvarsak@outlook.com>
2023-12-22 00:08:38 +0300

    added layouts on dashboard and settings page

---
+++ 01-routing-02-pages-and-layouts/src/app/settings/page.tsx
@@ -0,0 +1,10 @@
+import Link from "next/link";
+
+export default function Page() {
+  return (
+    <div style={{ display: "flex", flexDirection: "column", gap: 20 }}>
+      Settings Page
+      <Link href={"/"}>Back</Link>
+    </div>
+  );
+}
